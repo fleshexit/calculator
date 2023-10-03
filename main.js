493b168ca5db9ed1ddf56613400610c4f3a719b1
Charlie <chrlie.w@proton.me>
2023-10-03 17:59:38 +0100

    curved borders, basic logic template

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ const screen = document.querySelector('.screen');
 const keypad = document.querySelector('.keypad');
 const keys = document.querySelectorAll('.keypad btn');
 
-
+let equation = [];
 
 
 keypad.addEventListener('click', function(e) {
 
     let keyVal = e.target.textContent;
-    let screenVal = screen.textContent;    
 
     if (keyVal === 'AC') {
         screen.textContent = '';
@@ -16,15 +15,13 @@ keypad.addEventListener('click', function(e) {
     else if (keyVal === 'C') {
         screen.textContent = screen.textContent.slice(0, -1);
     }
+    else if (keyVal === '=') {
+        console.log(equation);
+        calculate();
+    }
     else {
         screen.textContent += keyVal;
+        equation.push(keyVal);
+
     }
-
-
-
 });
-
-
-function updateDisplay(x) {
-    screen.textContent = x;
-}
